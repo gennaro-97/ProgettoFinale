834d718e58b42e4905222ab1f0f5aefa39c3985c
gennaro-97 <58686645+gennaro-97@users.noreply.github.com>
2025-03-25 14:31:10 +0100

    Delete UtenteRepository

--- backend/src/main/java/com/example/backend/repositories/UtenteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.backend.models.Utente;
-
-public interface UtenteRepository extends JpaRepository<Utente, Long> {
-    
-}
